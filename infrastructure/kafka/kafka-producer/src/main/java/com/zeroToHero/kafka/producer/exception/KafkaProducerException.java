462867d6a50e12d4e21c994efe42352ee482d39c
Mehmet <mathdenizi@gmail.com>
2022-11-16 06:06:38 +0100

    kafka producer implementation is over

---
+++ infrastructure/kafka/kafka-producer/src/main/java/com/zeroToHero/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,8 @@
+package com.zeroToHero.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
