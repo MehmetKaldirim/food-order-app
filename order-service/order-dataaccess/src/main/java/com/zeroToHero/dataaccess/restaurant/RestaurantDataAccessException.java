cabe532c34784c6f1cba888993aa053734ba7c09
Mehmet <mathdenizi@gmail.com>
2022-11-19 00:08:16 +0100

    data access implementation for all is done

---
+++ order-service/order-dataaccess/src/main/java/com/zeroToHero/dataaccess/restaurant/RestaurantDataAccessException.java
@@ -0,0 +1,7 @@
+package com.zeroToHero.dataaccess.restaurant;
+
+public class RestaurantDataAccessException extends RuntimeException{
+    public RestaurantDataAccessException(String message) {
+        super(message);
+    }
+}
--- order-service/order-domain/order-domain-core/src/main/java/com/zeroToHero/order/service/domain/entity/Customer.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/zeroToHero/order/service/domain/entity/Customer.java
@@ -4,4 +4,10 @@ import com.zeroToHero.domain.entity.AggregateRoot;
 import com.zeroToHero.domain.valueobject.CustomerId;
 
 public class Customer extends AggregateRoot<CustomerId> {
+    public Customer() {
+    }
+
+    public Customer(CustomerId customerId) {
+        super.setId(customerId);
+    }
 }
